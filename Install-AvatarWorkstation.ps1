83b80a02a704c4e45e862a1b5e64c9296385fcd8
Alistair Young <avatar@arkane-systems.net>
2018-05-10 09:24:18 -0500

    To get URL

---
+++ Install-AvatarWorkstation.ps1
@@ -0,0 +1,40 @@
+﻿# BoxStarter setup script to initialize a workstation
+# with all of my foo.
+#
+# "I pity the foo!" - me
+#
+# Alistair Young <avatar@arkane-systems.net>, 2016-2018
+
+# USAGE:
+#
+# Install BoxStarter:
+#     . { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
+#
+# Run this script (elevated):
+#     Install-BoxstarterPackage -PackageName  -DisableReboots
+
+# Shut down UAC while the script runs
+Disable-UAC
+
+# -- Configure Windows features
+
+
+
+# -- Configure Windows settings
+
+
+# -- Fonts
+
+choco install inconsolata -y
+
+# -- Software
+
+
+# -- Reenable UAC
+
+Enable-UAC
+
+# -- Windows Updates
+
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
